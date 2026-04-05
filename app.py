87b62f77f922392fe0e2179017ce4ac15a1873ab
Aditya Mantri <153272907+AdityaMMantri@users.noreply.github.com>
2026-04-05 12:34:26 +0530

    Update app.py

--- app.py
+++ app.py
@@ -20,7 +20,7 @@ def home():
         elif action == "subtract":
             a = int(request.form.get("a", 0))
             b = int(request.form.get("b", 0))
-            result = f"Difference = {b-a}"
+            result = f"Difference = {a-b}"
 
     return render_template("index.html", result=result)
 
